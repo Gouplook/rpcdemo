096607a47d033b98843fccd28854b7da2aef07ee
yinjinlin <yinjinlin_uplook@163.com>
2021-01-05 18:06:57 +0800

    xx

---
+++ common/plugins/jaeger.go
@@ -0,0 +1,8 @@
+/********************************************
+
+@Author :yinjinlin<yinjinlin_uplook@163.com>
+@Time : 2021/1/4 11:23
+@Description:
+
+*********************************************/
+package plugins
---
+++ rpcinterface/client/poplar/student.go
@@ -0,0 +1,9 @@
+
+/********************************************
+
+@Author :yinjinlin<yinjinlin_uplook@163.com>
+@Time : 2021/1/4 11:02 
+@Description:
+
+*********************************************/
+package poplar
